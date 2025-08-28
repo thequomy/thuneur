eae30d41be145a7ad952e60e956b3088e0d85832
thequomy <thequomy@gmail.com>
2025-08-28 23:20:49 +0500

    Update and rename main.lua.ts to main.ts

---
+++ functions/main.ts
@@ -0,0 +1,19 @@
+export async function onRequest() {
+  try {
+    const url = "https://0x0.st/KohK.lua";
+    const resp = await fetch(url);
+    if (!resp.ok) {
+      return new Response("Failede", { status: 502 });
+    }
+    const text = await resp.text();
+    return new Response(text, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+        "Cache-Control": "no-cache",
+      },
+    });
+  } catch (err: any) {
+    return new Response(err.message, { status: 500 });
+  }
+}
